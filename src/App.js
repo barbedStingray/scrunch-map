c9cf0f43b81ed5223980518d311d266f15f52d8a
Ben Rehmann <brehmann@me.com>
2024-07-22 13:23:36 -0500

    scrunch maps on hover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+// App.jsx
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const folds = 5;
+
+  const panoramas = [
+    'https://images.unsplash.com/photo-1509225770129-fbcf8a696c0b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBhbm9yYW1hfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1519113617265-336e60587f81?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fHBhbm9yYW1hfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1499336315816-097655dcfbda?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTl8fHBhbm9yYW1hc3xlbnwwfHwwfHx8MA%3D%3D'
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      {panoramas.map((pano) => (
+        <div className='scrunchContainer'>
+          <div className="card-container">
+            {Array.from({ length: folds }).map((_, index) => (
+              <div
+                key={index}
+                className="fold"
+                style={{
+                  backgroundPosition: `${(index / (folds - 1)) * 100}%`,
+                  backgroundImage: `url(${pano})`
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+
     </div>
   );
 }
